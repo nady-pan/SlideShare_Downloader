cb070cbd863b1530a1ae129794cd3880cfcf85e8
nady <nady@pan.sitetag.us>
2015-08-03 11:16:34 +0800

    change url content to xxx

--- slideshare.py
+++ slideshare.py
@@ -3,10 +3,12 @@ import urllib.request
 import os
 
 pages = 93
-url = 'http://image.slidesharecdn.com/2-150728061037-lva1-app6892/95/-%d-638.jpg'
-filename = 'jpgs/%d_638.jpg'
+url = 'http://image.slidesharecdn.com/xxxxxxxxxxxxxxxxxxxxxxxxxxx/xx/-%d-xxx.jpg'
+filename = 'jpgs/%d_xxx.jpg'
 
 print(' %d pages downloading...' % pages)
+
+os.system('mkdir ./jpgs/')
 
 for i in range(1, pages):
     urllib.request.urlretrieve(url % i, filename % i)
